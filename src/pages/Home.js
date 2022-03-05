f7ba46cb6bcd8aabbf061e09b30714ea0dde221d
YungTesla <wouter.sluiter@hotmail.com>
2022-03-05 11:58:22 +0100

    update home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,6 @@
 import Hero from "./Home/Hero";
 import Collections from "./Home/Collections";
-import Rewards from "./Home/Rewards";
-import Royalties from "./Home/Royalties";
 // import Calculator from "./Home/Calculator";
-import Voting from "./Home/Voting";
 import Rarities from "./Home/Rarities";
 import Process from "./Home/Process";
 import Characteristics from "./Home/Characteristics";
